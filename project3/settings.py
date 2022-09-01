cf3a00a1800d28a4bf1b27dabb4c617337f31ecb
Maieza N Borges <maieza.borges@gmail.com>
2022-09-01 15:01:13 -0400

    changed db to postgresql and set up docker

--- project3/settings.py
+++ project3/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
